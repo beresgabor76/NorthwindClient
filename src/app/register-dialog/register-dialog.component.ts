07c67413015020dc287265dcceded4e10955edf7
beresgabor76 <beres.gabor.76@gmail.com>
2022-07-21 17:41:56 +0200

    refactor: registerForm validation

--- src/app/register-dialog/register-dialog.component.ts
+++ src/app/register-dialog/register-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, NgForm, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
-import { LoginDto } from '../_models/LoginDto';
 import { AuthService } from '../_services/auth.service';
 import { MessageService } from '../_services/message.service';
 
@@ -11,8 +11,15 @@ import { MessageService } from '../_services/message.service';
   styleUrls: ['./register-dialog.component.css']
 })
 export class RegisterDialogComponent implements OnInit, OnDestroy {
+
   loginSub: Subscription = this.authService.loginSubject.subscribe(
     tokenDto => { if (tokenDto.token) { this.dialogRef.close() } });
+
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required, passwordMatchValidator])
+  }, { validators: [passwordMatchValidator, passwordStrengthValidator] });
 
   constructor(public dialogRef: MatDialogRef<RegisterDialogComponent>, private authService: AuthService,
               private messageService: MessageService) { }
@@ -24,12 +31,59 @@ export class RegisterDialogComponent implements OnInit, OnDestroy {
     this.loginSub.unsubscribe();
   }
 
-  onSubmit(data: any) {
-    if (data.password === data.confirmPassword) {
-      this.authService.register(data.username, data.password);
+  onSubmit() {
+    if (this.registerForm.controls['username'].valid &&
+      this.registerForm.controls['password'].valid &&
+      this.registerForm.getError('passwordMatch') == true) {
+      this.authService.register(
+        this.registerForm.controls['username'].value,
+        this.registerForm.controls['password'].value);
     }
     else {
-      this.messageService.showError("Password fields do not match.");
+      this.messageService.showError('Please fill in the form properly.');      
     }
   }
+  
+  public hasError(controlName: string, errorName: string) {
+    return this.registerForm.controls[controlName].hasError(errorName);
+  }
+
+  public hasPasswordMatchError() {
+    if (this.registerForm.getError('noPasswordMatch') == true)  
+      return true;    
+    else
+      return false;
+  }
 }
+/*
+export const passwordStrengthValidator: ValidatorFn
+  = (control: AbstractControl): ValidationErrors | null => {
+
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+
+    const hasUpperCase = /[A-Z]+/.test(value);
+
+    const hasLowerCase = /[a-z]+/.test(value);
+
+    const hasNumeric = /[0-9]+/.test(value);
+
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+    return !passwordValid ? { passwordStrength: true } : { passwordStrength: false };
+  };
+  */
+
+export const passwordMatchValidator: ValidatorFn
+  = (control: AbstractControl): ValidationErrors | null => {
+    
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    return (password?.value != confirmPassword?.value) ? { noPasswordMatch: true } : { passwordMatch: true };
+  };
+  
+
